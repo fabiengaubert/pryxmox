8d13b76e9059d6f813fcf0c59a521d1503d2c919
fabiengaubert <fab.gaubert@gmail.com>
2025-08-11 23:22:12 +0200

    add vm and node listing and printing

--- proxmox/proxmox.py
+++ proxmox/proxmox.py
@@ -40,3 +40,60 @@ def get_version_proxmox(proxmox: ProxmoxAPI) -> bool:
         str: Version string of the Proxmox server (e.g., "7.1-15").
     """
     return proxmox.version.get()['version']
+
+
+def list_vms(proxmox: ProxmoxAPI, node: str) -> List[dict]:
+    """
+    Lists all virtual machines on a specified Proxmox node.
+
+    Args:
+        proxmox (ProxmoxAPI): An authenticated ProxmoxAPI instance.
+        node (str): The name of the Proxmox node to query.
+
+    Returns:
+        List[dict]: A list of dictionaries containing VM details.
+    """
+    return proxmox.nodes(node).qemu.get()
+
+
+def list_nodes(proxmox: ProxmoxAPI) -> List[str]:
+    """
+    Lists all nodes in the Proxmox cluster.
+
+    Args:
+        proxmox (ProxmoxAPI): An authenticated ProxmoxAPI instance.
+
+    Returns:
+        List[str]: A list of node names in the Proxmox cluster.
+    """
+    return [node['node'] for node in proxmox.nodes.get()]
+
+
+def print_vm_list(vms: List[dict]) -> None:
+    """
+    Prints a formatted list of virtual machines.
+
+    Args:
+        vms (List[dict]): A list of dictionaries containing VM details.
+    """
+    print(f"{'VM ID':<10} {'Name':<30} {'Status':<10}")
+    print("=" * 50)
+    for vm in vms:
+        print(f"{vm['vmid']:<10} {vm['name']:<30} {vm['status']:<10}")
+
+
+def print_all_vms(proxmox: ProxmoxAPI) -> None:
+    """
+    Prints all virtual machines across all nodes in the Proxmox cluster.
+
+    Args:
+        proxmox (ProxmoxAPI): An authenticated ProxmoxAPI instance.
+    """
+    nodes = list_nodes(proxmox)
+    print()
+    for node in nodes:
+        print(f"Node: {node}")
+        print()
+        vms = list_vms(proxmox, node)
+        print_vm_list(vms)
+        print("\n")
--- pryxmox.py
+++ pryxmox.py
@@ -1,11 +1,12 @@
 from env_loader import load_proxmox_env
-from proxmox.proxmox import connect_proxmox, get_version_proxmox
+from proxmox.proxmox import connect_proxmox, get_version_proxmox, print_all_vms
 
 
 def main():
     config_proxmox = load_proxmox_env()
     proxmox = connect_proxmox(config_proxmox)
     print(f"Proxmox version: {get_version_proxmox(proxmox)}")
+    print_all_vms(proxmox)
 
 
 if __name__ == "__main__":
