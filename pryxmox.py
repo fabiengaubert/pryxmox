5a8369568072a197a5f7ae502b082175f3bac4d5
fabiengaubert <fab.gaubert@gmail.com>
2025-08-11 23:26:04 +0200

    rename proxmox_api

--- pryxmox.py
+++ pryxmox.py
@@ -1,12 +1,11 @@
 from env_loader import load_proxmox_env
-from proxmox.proxmox import connect_proxmox, get_version_proxmox, print_all_vms
+from proxmox.proxmox_api import connect_proxmox, get_version_proxmox, print_all_vms
 
 
 def main():
     config_proxmox = load_proxmox_env()
     proxmox = connect_proxmox(config_proxmox)
     print(f"Proxmox version: {get_version_proxmox(proxmox)}")
-    print_all_vms(proxmox)
 
 
 if __name__ == "__main__":
